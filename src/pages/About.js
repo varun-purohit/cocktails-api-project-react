14f074a41fd4e9670aace770cdafcd3a08344b2d
varun-purohit <varunpurohit48@gmail.com>
2022-10-06 22:28:13 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
+        dicta pariatur possimus natus debitis laboriosam ea ullam fugiat?
+        Aliquid quibusdam hic consequatur deleniti iure ab repudiandae pariatur
+        quo, veniam nihil, fugit fugiat nam ut, porro ullam magnam ad tenetur.
+        Alias.
+      </p>
+    </section>
+  );
+};
+
+export default About;
